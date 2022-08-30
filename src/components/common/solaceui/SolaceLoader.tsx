435c3e70561d3be79c81c26962602e274528608c
Ankit Negi <ankitn1311@gmail.com>
2022-08-30 21:53:48 +0530

    git cache cleaned

---
+++ src/components/common/solaceui/SolaceLoader.tsx
@@ -0,0 +1,26 @@
+import React, {FC, ReactNode} from 'react';
+import {StyleProp, View, ViewStyle} from 'react-native';
+import SolaceText from './SolaceText';
+
+type Props = {
+  text: string;
+  children?: ReactNode;
+  style?: StyleProp<ViewStyle>;
+};
+
+const SolaceLoader: FC<Props> = ({text, children, style, ...textProps}) => {
+  return (
+    <View
+      style={[
+        {flex: 1, justifyContent: 'center', alignItems: 'center'},
+        style || {},
+      ]}>
+      <SolaceText variant="normal" type="secondary" {...textProps}>
+        {text}
+        {children}
+      </SolaceText>
+    </View>
+  );
+};
+
+export default SolaceLoader;
