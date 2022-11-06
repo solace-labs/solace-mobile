1fb013ece1ffb5702d8ae8b141cd9512be8fadc0
Ankit Negi <ankitn1311@gmail.com>
2022-11-06 15:59:22 +0530

    ramp integration complete

--- src/components/screens/wallet/home/Buy.tsx
+++ src/components/screens/wallet/home/Buy.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {SolaceSDK} from 'solace-sdk';
 
 import SolaceContainer from '../../../common/solaceui/SolaceContainer';
@@ -7,6 +7,7 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {useNavigation} from '@react-navigation/native';
 import {WalletStackParamList} from '../../../../navigation/Home/Home';
 import {WebView} from 'react-native-webview';
+import {GlobalContext} from '../../../../state/contexts/GlobalContext';
 
 type WalletScreenProps = NativeStackScreenProps<WalletStackParamList, 'Buy'>;
 
@@ -16,6 +17,7 @@ export type WalletDataType = Awaited<
 
 const Buy = () => {
   const navigation = useNavigation<WalletScreenProps['navigation']>();
+  const {state} = useContext(GlobalContext);
 
   const handleGoBack = () => {
     navigation.goBack();
@@ -29,7 +31,11 @@ const Buy = () => {
         text="buy"
         startClick={handleGoBack}
       />
-      <WebView source={{uri: 'https://ramp.network/buy'}} />
+      <WebView
+        source={{
+          uri: `https://buy.ramp.network/?userAddress=${state.sdk?.wallet}&swapAsset=SOLANA_SOL,SOLANA_USDC,SOLANA_USDT&hostLogoUrl=https://security.solace.money/solace.png&hostAppName=solace&fiatCurrency=GBP&selectedCountryCode=IND`,
+        }}
+      />
     </SolaceContainer>
   );
 };
